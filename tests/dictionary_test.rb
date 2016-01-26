1e4374edaf8bcdec26fd6096fcfbfdb6fd24f396
Thorin Butler <thorin.butler@gmail.com>
2016-01-26 13:55:06 -0600

    WIP_tests

--- tests/dictionary_test.rb
+++ tests/dictionary_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 require 'dictionary'
 
 class DictionaryTest < Minitest::Test
-  # Your test methods go here.
+  def test_dictionary
+    sample_word = "one"
+    assert_equal(true, valid_word?(sample_word))
+  end
+
+  def test_not_in_dictionary
+    sample_word = "dynusour"
+    assert_equal(false, valid_word?(sample_word))
+  end
 end
 
 # Remember, the names of all test methods should begin with "test_".
